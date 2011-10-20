45b468061e7f1260281aeb54973e0492901674e4
Jacob Rask <jacob@jacobrask.net>
2011-10-20 11:28:50 +0200

    Define some functions to load scripts and check session ID

--- web/scripts/app.coffee
+++ web/scripts/app.coffee
@@ -1,3 +1,34 @@
 $ ->
-    $.get '/views/login.js', ->
+    # loads scripts without global ajax settings
+    loadScript = (script, callback) ->
+        tmpData = $.ajaxSettings['data']
+        $.ajaxSettings['data'] = undefined
+        $.get script, ->
+            $.ajaxSettings['data'] = tmpData
+            callback()
+
+    $.ajaxSetup
+        url: '/content/interface'
+        data:
+            return_type: 'json'
+
+    getSID = (callback) ->
+        if $.cookie 'session_id'
+            callback $.cookie 'session_id'
+        else
+            $.ajax
+                data:
+                    req_type: 'auth'
+                    action: 'get_sid'
+                success: (json) ->
+                    $.cookie 'session_id', json['sid']
+                    callback json['sid']
+
+    getSID (sid) ->
+        $.ajaxSetup
+            data:
+                sid: sid
+
+    loadScript '/views/login.js', ->
         $('body').html templates.login()
+
--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,6 +1,45 @@
 (function() {
   $(function() {
-    return $.get('/views/login.js', function() {
+    var getSID, loadScript;
+    loadScript = function(script, callback) {
+      var tmpData;
+      tmpData = $.ajaxSettings['data'];
+      $.ajaxSettings['data'] = void 0;
+      return $.get(script, function() {
+        $.ajaxSettings['data'] = tmpData;
+        return callback();
+      });
+    };
+    $.ajaxSetup({
+      url: '/content/interface',
+      data: {
+        return_type: 'json'
+      }
+    });
+    getSID = function(callback) {
+      if ($.cookie('session_id')) {
+        return callback($.cookie('session_id'));
+      } else {
+        return $.ajax({
+          data: {
+            req_type: 'auth',
+            action: 'get_sid'
+          },
+          success: function(json) {
+            $.cookie('session_id', json['sid']);
+            return callback(json['sid']);
+          }
+        });
+      }
+    };
+    getSID(function(sid) {
+      return $.ajaxSetup({
+        data: {
+          sid: sid
+        }
+      });
+    });
+    return loadScript('/views/login.js', function() {
       return $('body').html(templates.login());
     });
   });
