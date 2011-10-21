56f904175bc865b4f857828aa5674d5099822ddc
Jacob Rask <jacob@jacobrask.net>
2011-10-21 10:09:02 +0200

    Keep "synchronous app flow" in $ -> and the rest outside

--- web/scripts/app.coffee
+++ web/scripts/app.coffee
@@ -1,48 +1,73 @@
-$ ->
-    renderView = (view) ->
-        $.Deferred ->
-            $.when $.get('/views/' + view + '.js').done =>
-                $('#main').html templates[view]()
-                this.resolve()
-        .promise()
+# setup defaults
+ajaxDefault =
+    data:
+        return_type: 'json'
+$.ajaxSetup
+    url: '/content/interface'
+
+renderView = (view) ->
+    $.Deferred ->
+        $.when $.get('/views/' + view + '.js').done =>
+            $('#main').html templates[view]()
+            this.resolve()
+    .promise()
     
-    ajaxDefault =
-        data:
-            return_type: 'json'
-    $.ajaxSetup
-        url: '/content/interface'
+# get session id from cookie or server
+getSID = (callback) ->
+    if $.cookie('session_id')?
+        callback $.cookie 'session_id'
+    else
+        $.ajax
+            data: $.extend
+                req_type: 'auth'
+                action: 'get_sid'
+                ajaxDefault['data']
+            success: (json) ->
+                $.cookie 'session_id', json['sid']
+                callback json['sid']
 
-    # get session id from cookie or server
-    getSID = (callback) ->
-        if $.cookie('session_id')?
-            callback $.cookie 'session_id'
-        else
-            $.ajax
-                data: $.extend
+# get config key from cookie or server    
+getConfig = (key, callback) ->
+    if $.cookie('config')?
+        config = JSON.parse $.cookie 'config'
+        callback config[key]
+    else
+        $.ajax
+            data: $.extend
+                req_type: 'auth'
+                action: 'get_config'
+                ajaxDefault['data']
+            data: data
+            success: (json) ->
+                config = json['config']
+                $.cookie 'config', JSON.stringify config
+                callback config[key]
+
+handleLogin = ->
+    $('#login').submit ->
+        $.ajax
+            data: $.extend
+                req_type: 'auth'
+                action: 'get_token'
+                ajaxDefault['data']
+            success: (json) ->
+                token = json['token']
+                username = $('#username').val()
+                password = $('#password').val()
+                passwordMd5 = $.md5(token + password)
+                data = $.extend
                     req_type: 'auth'
-                    action: 'get_sid'
+                    action: 'login'
+                    username: username
+                    password: passwordMd5
                     ajaxDefault['data']
-                success: (json) ->
-                    $.cookie 'session_id', json['sid']
-                    callback json['sid']
+                $.ajax
+                    data: data
+                    success: (json) ->
+                        console.log json
+        false
 
-    # get config key from cookie or server    
-    getConfig = (key, callback) ->
-        if $.cookie('config')?
-            config = JSON.parse $.cookie 'config'
-            callback config[key]
-        else
-            $.ajax
-                data: $.extend
-                    req_type: 'auth'
-                    action: 'get_config'
-                    ajaxDefault['data']
-                data: data
-                success: (json) ->
-                    config = json['config']
-                    $.cookie 'config', JSON.stringify config
-                    callback config[key]
-
+$ ->
     getSID (sid) ->
         ajaxDefault['data']['sid'] = sid
         getConfig 'accounts', (accounts) ->
@@ -56,27 +81,3 @@ $ ->
                         $.get '/scripts/jquery.md5.js'
                     ).done ->
                        handleLogin()
-
-    handleLogin = ->
-        $('#login').submit ->
-            $.ajax
-                data: $.extend
-                    req_type: 'auth'
-                    action: 'get_token'
-                    ajaxDefault['data']
-                success: (json) ->
-                    token = json['token']
-                    username = $('#username').val()
-                    password = $('#password').val()
-                    passwordMd5 = $.md5(token + password)
-                    data = $.extend
-                        req_type: 'auth'
-                        action: 'login'
-                        username: username
-                        password: passwordMd5
-                        ajaxDefault['data']
-                    $.ajax
-                        data: data
-                        success: (json) ->
-                            console.log json
-            false
--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,60 +1,90 @@
 (function() {
+  var ajaxDefault, getConfig, getSID, handleLogin, renderView;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  $(function() {
-    var ajaxDefault, getConfig, getSID, handleLogin, renderView;
-    renderView = function(view) {
-      return $.Deferred(function() {
-        return $.when($.get('/views/' + view + '.js').done(__bind(function() {
-          $('#main').html(templates[view]());
-          return this.resolve();
-        }, this)));
-      }).promise();
-    };
-    ajaxDefault = {
-      data: {
-        return_type: 'json'
-      }
-    };
-    $.ajaxSetup({
-      url: '/content/interface'
+  ajaxDefault = {
+    data: {
+      return_type: 'json'
+    }
+  };
+  $.ajaxSetup({
+    url: '/content/interface'
+  });
+  renderView = function(view) {
+    return $.Deferred(function() {
+      return $.when($.get('/views/' + view + '.js').done(__bind(function() {
+        $('#main').html(templates[view]());
+        return this.resolve();
+      }, this)));
+    }).promise();
+  };
+  getSID = function(callback) {
+    if ($.cookie('session_id') != null) {
+      return callback($.cookie('session_id'));
+    } else {
+      return $.ajax({
+        data: $.extend({
+          req_type: 'auth',
+          action: 'get_sid'
+        }, ajaxDefault['data']),
+        success: function(json) {
+          $.cookie('session_id', json['sid']);
+          return callback(json['sid']);
+        }
+      });
+    }
+  };
+  getConfig = function(key, callback) {
+    var config;
+    if ($.cookie('config') != null) {
+      config = JSON.parse($.cookie('config'));
+      return callback(config[key]);
+    } else {
+      return $.ajax({
+        data: $.extend({
+          req_type: 'auth',
+          action: 'get_config'
+        }, ajaxDefault['data']),
+        data: data,
+        success: function(json) {
+          config = json['config'];
+          $.cookie('config', JSON.stringify(config));
+          return callback(config[key]);
+        }
+      });
+    }
+  };
+  handleLogin = function() {
+    return $('#login').submit(function() {
+      $.ajax({
+        data: $.extend({
+          req_type: 'auth',
+          action: 'get_token'
+        }, ajaxDefault['data']),
+        success: function(json) {
+          var data, password, passwordMd5, token, username;
+          token = json['token'];
+          username = $('#username').val();
+          password = $('#password').val();
+          passwordMd5 = $.md5(token + password);
+          data = $.extend({
+            req_type: 'auth',
+            action: 'login',
+            username: username,
+            password: passwordMd5
+          }, ajaxDefault['data']);
+          return $.ajax({
+            data: data,
+            success: function(json) {
+              return console.log(json);
+            }
+          });
+        }
+      });
+      return false;
     });
-    getSID = function(callback) {
-      if ($.cookie('session_id') != null) {
-        return callback($.cookie('session_id'));
-      } else {
-        return $.ajax({
-          data: $.extend({
-            req_type: 'auth',
-            action: 'get_sid'
-          }, ajaxDefault['data']),
-          success: function(json) {
-            $.cookie('session_id', json['sid']);
-            return callback(json['sid']);
-          }
-        });
-      }
-    };
-    getConfig = function(key, callback) {
-      var config;
-      if ($.cookie('config') != null) {
-        config = JSON.parse($.cookie('config'));
-        return callback(config[key]);
-      } else {
-        return $.ajax({
-          data: $.extend({
-            req_type: 'auth',
-            action: 'get_config'
-          }, ajaxDefault['data']),
-          data: data,
-          success: function(json) {
-            config = json['config'];
-            $.cookie('config', JSON.stringify(config));
-            return callback(config[key]);
-          }
-        });
-      }
-    };
-    getSID(function(sid) {
+  };
+  $(function() {
+    return getSID(function(sid) {
       ajaxDefault['data']['sid'] = sid;
       return getConfig('accounts', function(accounts) {
         return getConfig('logged_in', function(loggedIn) {
@@ -70,35 +100,5 @@
         });
       });
     });
-    return handleLogin = function() {
-      return $('#login').submit(function() {
-        $.ajax({
-          data: $.extend({
-            req_type: 'auth',
-            action: 'get_token'
-          }, ajaxDefault['data']),
-          success: function(json) {
-            var data, password, passwordMd5, token, username;
-            token = json['token'];
-            username = $('#username').val();
-            password = $('#password').val();
-            passwordMd5 = $.md5(token + password);
-            data = $.extend({
-              req_type: 'auth',
-              action: 'login',
-              username: username,
-              password: passwordMd5
-            }, ajaxDefault['data']);
-            return $.ajax({
-              data: data,
-              success: function(json) {
-                return console.log(json);
-              }
-            });
-          }
-        });
-        return false;
-      });
-    };
   });
 }).call(this);
